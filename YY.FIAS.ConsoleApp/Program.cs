8daeab62153ee708f2df673698c87858a5012f08
YPermitin <ypermitin@yandex.ru>
2020-07-11 17:02:52 +0500

    Добавил пример консольного приложения для загрузки файлов данных ФИАС

---
+++ YY.FIAS.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using YY.FIAS.Loader;
+
+namespace YY.FIAS.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начало загрузки данных ФИАС...");
+
+            MainAsync(args).Wait();
+
+            Console.WriteLine("Загрузка завершена.");
+            Console.ReadKey();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            string workDirectory = Path.Combine(Environment.CurrentDirectory, "TestData"); ;
+            IFIASLoader loader = new FIASLoader();
+            DownloadFileInfo lastInfo = await loader.GetLastDownloadFileInfo();
+
+            await Task.WhenAll(
+                lastInfo.SaveFiasDeltaDbToDirectory(workDirectory),
+                lastInfo.SaveFiasDeltaXmlToDirectory(workDirectory),
+                lastInfo.SaveFiasCompleteDbfToDirectory(workDirectory),
+                lastInfo.SaveFiasCompleteXmlToDirectory(workDirectory),
+                lastInfo.SaveKladr47ZToDirectory(workDirectory),
+                lastInfo.SaveKladr4ArjToDirectory(workDirectory)
+            );
+        }
+    }
+}
