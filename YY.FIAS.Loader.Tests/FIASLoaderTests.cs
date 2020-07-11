34b1e0fe027eca5761cb1c875c3d0a825518384f
YPermitin <ypermitin@yandex.ru>
2020-07-11 09:32:18 +0500

    Добавил unit-тесты для основных методов класса FIASLoader

---
+++ YY.FIAS.Loader.Tests/FIASLoaderTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace YY.FIAS.Loader.Tests
+{
+    public class FIASLoaderTests
+    {
+        [Fact]
+        public async void GetLastDownloadFileInfoTest()
+        {
+            IFIASLoader loader = new FIASLoader();
+            DownloadFileInfo lastInfo = await loader.GetLastDownloadFileInfo();
+
+            Assert.NotNull(lastInfo);
+        }
+
+        [Fact]
+        public async void GetAllDownloadFileInfoTest()
+        {
+            IFIASLoader loader = new FIASLoader();
+            IReadOnlyList<DownloadFileInfo> allInfo = await loader.GetAllDownloadFileInfo();
+
+            Assert.NotNull(allInfo);
+            Assert.True(allInfo.Count > 0);
+        }
+    }
+}
